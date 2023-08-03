260e86b4adc894851e815dca672eaa8a6378c091
Shohobiddinn <shohobiddinibrohimov@gmail.com>
2023-08-03 11:09:31 +0500

    api

--- app.vue
+++ app.vue
@@ -1,15 +1,7 @@
 <template>
   <div>
-    <pre>productObj: dsfwesdo0kp{{ productObj }}</pre>
-    <pre> rawObj:{{ rawObj }}</pre>
+    <NuxtLayout />
   </div>
-
 </template>
 <script setup>
-const productObj = ref(null);
-async function getFaceApi(){
-  const data =await $fetch("https://fakestoreapi.com/products");
-  productObj.value = data;
-}
-getFaceApi()
 </script>
